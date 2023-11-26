3733de8940ac8fc401e51f61b52046b9d9568796
lsjuandiego <lsjuandiegoo@gmail.com>
2023-11-26 14:54:35 -0500

    controlador y servicio carga archivos

---
+++ app/controllers/file_controller.py
@@ -0,0 +1,42 @@
+from app.services.file_service import FileService
+from app.utils.utils import Utils
+
+'''
+clase para controlar conexiones de servicios para manejo de archivos 
+'''
+
+
+class FileController:
+
+    def __init__(self):
+        self.file_service = FileService()
+        self.utils = Utils()
+
+# metodo asincrono para cargar el archivo desde el explorador
+
+    async def upload_file(self, file):
+        if not file:
+            return self.utils.prueba(msg='No se recibió ningún archivo.'), 400
+
+        if file.filename == '':
+            return self.utils.prueba(msg='No se seleccionó ningún archivo.'), 400
+
+        try:
+            if await self.file_service.guardar_archivo(file, "original", './app/files/sets/'):
+                return self.utils.prueba(msg='Archivo guardado correctamente'), 201
+        except Exception as e:
+            return self.utils.prueba(msg=f'Error al guardar el archivo: {str(e)}'), 500
+
+# metodo para devolver el nombre de los archivos
+    def get_all_names_files(self):
+        try:
+            return (self.utils.prueba(msg='Lista de nombres de archivos', datos=self.file_service.obtener_nombres_archivos())), 200
+        except Exception as e:
+            return self.utils.prueba(msg=f'Error al obtener los nombres de los archivos: {str(e)}'), 500
+
+    def obtener_datasets(self):
+        datasets = self.file_service.obtener_datasets()
+        if datasets:
+            return self.utils.prueba(msg='Lista de nombres de datasets disponibles', datos=datasets), 200
+        else:
+            return self.utils.prueba(msg='No hay datasets disponibles'), 404
---
+++ app/services/file_service.py
@@ -0,0 +1,158 @@
+from datetime import datetime
+import os
+import glob
+import pandas as pd
+from app.utils.utils import Utils
+
+'''
+clase que define los servicios para el manejo de archivos
+'''
+
+
+class FileService:
+
+    def welcome(self):
+        return 'Bienvenido a la API de ML.'
+
+    '''
+    Guarda un archivo en la ubicación especificada. Genera un nombre único para el archivo basado en la fecha y hora actual. 
+    Escribe los datos del archivo en el disco y guarda la información del archivo en un objeto JSON utilizando el servicio de MongoDB.
+    '''
+    async def guardar_archivo(self, file, version, ubicacion):
+        try:
+            filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '__' + file.filename
+            # Crear la carpeta si no existe
+            os.makedirs(ubicacion, exist_ok=True)
+            directory = os.path.join(ubicacion, filename)
+            # print(directory)
+            with open(directory, 'wb') as f:
+                f.write(await file.read())
+            self.guardar_archivo_json(version, filename, 'sets', 'Dataset')
+            return 'Archivo guardado correctamente.'
+        except FileNotFoundError as e:
+            return f'Error al guardar el archivo: {str(e)}'
+
+    '''
+     Obtiene los nombres de los archivos presentes en una carpeta específica. Devuelve una lista de los nombres de los archivos encontrados.
+     '''
+
+    def obtener_nombres_archivos(self):
+        ruta_carpeta = os.path.join('app', 'files')
+        nombres_archivos = []
+
+        try:
+            for nombre_archivo in os.listdir(ruta_carpeta):
+                if os.path.isfile(os.path.join(ruta_carpeta, nombre_archivo)):
+                    nombres_archivos.append(nombre_archivo)
+
+            return nombres_archivos
+        except Exception as e:
+            print(f'Error al obtener los nombres de los archivos: {str(e)}')
+            return []
+
+    '''
+    Obtiene la ruta del archivo más reciente en la ubicación especificada. Filtra los archivos según su extensión (.xlsx, .csv, .png, .jpg) y devuelve la ruta del archivo más reciente.
+    '''
+
+    def obtener_ultimo_archivo(self, ubicacion):
+        try:
+            ruta_carpeta = os.path.join('app', 'files', ubicacion)
+            if 'imgs' in ubicacion:
+                archivo = glob.glob(ruta_carpeta + '.png') + \
+                    glob.glob(ruta_carpeta + '.jpg')
+            else:
+                archivo = glob.glob(os.path.join(
+                    ruta_carpeta, '*.xlsx')) + glob.glob(os.path.join(ruta_carpeta, '*.csv'))
+            if archivo:
+                archivo_mas_reciente = max(archivo, key=os.path.getctime)
+                # print(archivo_mas_reciente)
+                archivo_mas_reciente = archivo_mas_reciente.replace('/', '\\')
+                return archivo_mas_reciente
+            else:
+                return None
+        except FileNotFoundError as e:
+            print(f'Error al obtener el ultimo archivo: {str(e)}')
+            return None
+
+    '''
+    Verifica que el dataset elegido exista en la base de datos.
+    '''
+
+    def verificar_dataset(self, nombre_dataset: str):
+        datasets = self.mongo_service.obtener_nombres_dataset('Dataset')
+        if datasets:
+            for elemento in datasets:
+                if nombre_dataset.lower() in elemento or nombre_dataset.upper() in elemento or nombre_dataset.capitalize() in elemento:
+                    return True
+        print('No se encontrón datasets')
+        return False
+
+    def obtener_datasets(self):
+        datasets = self.mongo_service.obtener_nombres_dataset('Dataset')
+        if datasets:
+            return datasets
+        return None
+
+    '''
+    Convierte el archivo más reciente en la ubicación especificada (excel o csv) a un objeto JSON. Lee los datos del archivo utilizando pandas 
+    y los convierte en un diccionario JSON con los títulos de las columnas y los valores de cada fila.
+    '''
+
+    def convertir_archivo_a_json(self, version, filename, ubicacion):
+        try:
+            ultimo_archivo = self.obtener_ultimo_archivo(ubicacion)
+            if ultimo_archivo:
+                ruta_archivo = os.path.join(ultimo_archivo)
+                # print(ultimo_archivo)
+                # print(ruta_archivo)
+                if ultimo_archivo.endswith('.xlsx'):
+                    df = pd.read_excel(ultimo_archivo, engine='openpyxl')
+                elif ultimo_archivo.endswith('.csv'):
+                    df = pd.read_csv(ultimo_archivo, sep=';')
+                titulos = list(df.columns)
+                valores = df.to_dict(orient='records')
+                # print(titulos)
+                # print(valores)
+                nombre_dataset = filename.split("__")[1]
+                nombre_dataset = nombre_dataset.split(".")[0]
+                datos_json = {'nombreDoc': filename, 'nombre_dataset': nombre_dataset,
+                              'version': version, 'titulos': titulos, 'valores': valores}
+                return datos_json
+            else:
+                print('No se encontrón archivos Excel')
+                return None
+        except Exception as e:
+            print(f'Error al convertir el archivo Excel a JSON: {str(e)}')
+            return None
+
+    '''
+    Convierte el archivo más reciente en la ubicación especificada a un objeto JSON y lo guarda en una colección específica de MongoDB utilizando el servicio de MongoDB.
+    '''
+
+    def guardar_archivo_json(self, version, filename, ubicacion, coleccion):
+        try:
+            json = self.convertir_archivo_a_json(version, filename, ubicacion)
+            if json:
+                self.mongo_service.guardar_json(json, coleccion)
+                # print("ME CONECTÉ")
+                print('Archivo JSON guardado correctamente.')
+            else:
+                print("NO HAY JSON")
+                return None
+
+        except Exception as e:
+            print(f'Error al guardar el archivo JSON: {str(e)}')
+
+    '''
+    Realiza una copia de un DataFrame modificado en un archivo Excel. Guarda el DataFrame en un archivo Excel en la carpeta 
+    "cleanData" con un nombre que incluye la acción realizada (por ejemplo, 'accion_nombre_archivo.xlsx').
+    '''
+
+    def _copia_excel(self, df, nombre_archivo, accion):
+        try:
+            ruta_archivo = './app/files/cleanData/' + \
+                accion+os.path.basename(nombre_archivo)
+            df.to_excel(ruta_archivo, index=False)
+            return "Se ha guardado una copia del DataFrame modificado en un archivo Excel, en " + ruta_archivo
+        except Exception as e:
+            return f'Error al guardar el documento: {str(e)}'
